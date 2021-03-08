a7f5050cd288094c7b57a2c494532a97d9590b03
Giovanni Blangiardi <g.blangiardi@live.it>
2021-03-08 21:58:32 +0100

    Versione 0.1

--- course_scores/course_scores.js
+++ course_scores/course_scores.js
@@ -1,3 +1,35 @@
 "use strict";
 
-function Exam()
+function Exam(code, name, cfu, score, laude, date){
+    this.code=code;
+    this.name=name;
+    this.cfu=cfu;
+    this.score=score;
+
+    if(this.score === 30)
+        this.laude=laude;
+    else
+        this.laude=false;
+    
+    this.date=date;
+    
+}
+
+function ExamList(){
+    this.exams = [];
+
+    this.add = (ex) => (this.exams.push(ex));
+
+    this.printAll = () => (this.exams.forEach((s) => (console.log(s))));
+
+}
+
+let wa = new Exam("231hx", "Web Applications 1", 6, 30, true, "2021-06-27");
+let sdp = new Exam("3xjk5", "System and Device Programming", 10, 18, true, "2030-09-22");
+
+let myExams = new ExamList();
+myExams.add(wa);
+myExams.add(sdp);
+
+myExams.printAll();
+
